d195dc44d5fdd637897db2755c5ac570db178153
Nilesh Bhagwat Koli <153268077+KoliNilesh@users.noreply.github.com>
2024-02-23 02:45:52 +0530

    Add files via upload

---
+++ program111.java
@@ -0,0 +1,18 @@
+class program 111
+{
+	public static void main(String args[])
+    {
+     int n=5;
+     for (int i=1;i<=n;i++)
+     {
+      for (int j=1j<=1;j++)
+      {
+        if(j%2!=0)
+        System.out.print(0);
+        else
+        System.out.print(1);
+      }
+      System.out.println();
+     }
+    }
+}
